22bda93d8a14783ac9f166144efb36c442fa9fe7
Samuel Gustafsson <samuelgustafsson890623@gmail.com>
2021-05-18 22:40:29 +0200

    add password and username to url

--- src/services/WiseNetWaveDataService/config.ts
+++ src/services/WiseNetWaveDataService/config.ts
@@ -5,11 +5,7 @@ interface Config {
 }
 
 export const config: Config = {
-  url: "http://127.0.0.1:7001/api/",
+  url: "http://admin:Eteam1603@192.168.10.203:7001/api/",
   camID: "48cebdbb-cfa7-4773-1eed-eb984057e7d7",
   tag: "Visitor"
 };
-
-const tjenna = "TJENNA";
-
-console.log("test", tjenna);
